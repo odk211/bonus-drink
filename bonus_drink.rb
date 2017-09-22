2c620a99af2967a685ffe4fa184fff2034ab4468
odk211 <tomoki.odaka@gmail.com>
2017-09-22 14:36:40 +0900

    add implementation and test

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,7 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    raise ArgumentError.new('amount should be 0 or a positive value') if amount.negative?
+    return 0 if amount.zero?
+    amount + (amount - 1)/2
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  it { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  it { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  it { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  it { expect { BonusDrink.total_count_for(-1) }.to raise_error ArgumentError }
 end
