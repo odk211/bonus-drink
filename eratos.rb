4f4e1a45c85f9dea509961c1102cb93bef5f224c
odk211 <tomoki.odaka@gmail.com>
2017-09-22 16:29:46 +0900

    imprement eratos

--- eratos.rb
+++ eratos.rb
@@ -1,5 +1,21 @@
 class Eratos
-  def initialize(number)
-    @number = number
+  def initialize(limit)
+    @limit = limit.to_i
   end
+
+  def prime_numbers
+    Enumerator.new do |primes|
+      numbers = (2..@limit).to_a
+      up_to = Math.sqrt(@limit)
+
+      until numbers.empty?
+        prime = numbers.shift
+        primes << prime
+        if prime <= up_to
+          numbers.reject! { |n| (n % prime).zero? }
+        end
+      end
+    end.to_a
+  end
+
 end
